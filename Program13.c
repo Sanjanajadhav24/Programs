81f344097fc0bf2334f4056978e3bd39d0cf2008
Sanjanajadhav24 <Sanjana24jadhav@gmail.com>
2025-09-29 14:14:29 +0530

    Create Program13.c

---
+++ Program13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, result;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    result = a * b;
+    printf("Product = %.2f\n", result);
+
+    return 0;
+}
