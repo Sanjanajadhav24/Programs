0ec4b574d56a49c2a343777b92effb3b4094759c
Sanjanajadhav24 <Sanjana24jadhav@gmail.com>
2025-09-29 21:31:24 +0530

    Create Program53.c

---
+++ Program53.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    do {
+        count++;
+        num /= 10;
+    } while (num != 0);
+
+    printf("Number of digits = %d", count);
+    return 0;
+}
