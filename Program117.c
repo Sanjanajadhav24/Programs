da3f458ae4b5f52a5274e5e57404a8fc27f6fd70
Sanjanajadhav24 <Sanjana24jadhav@gmail.com>
2025-10-03 09:23:48 +0530

    Create Program117.c

---
+++ Program117.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    scanf("%s", str);
+    for(int i=0; str[i]!='\0'; i++) freq[(int)str[i]]++;
+    for(int i=0;i<256;i++) {
+        if(freq[i]>0) printf("%c : %d\n", i, freq[i]);
+    }
+    return 0;
+}
