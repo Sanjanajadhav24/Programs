f6ac46a8e2779be1e0d112c1cabedf2ccb4fde00
Sanjanajadhav24 <Sanjana24jadhav@gmail.com>
2025-10-30 14:25:47 +0530

    Create Program 195.c

---
+++ Program 195.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int r,c; scanf("%d%d",&r,&c); int A[r][c];
+    for(int i=0;i<r;i++) for(int j=0;j<c;j++) scanf("%d",&A[i][j]);
+    for(int j=0;j<c;j++){
+        for(int i=0;i<r;i++) printf("%d ", A[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
